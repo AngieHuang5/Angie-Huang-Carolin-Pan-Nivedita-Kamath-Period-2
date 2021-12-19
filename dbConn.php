16511e335e10602b18b6094d36c09769f842f78d
AngieHuang5 <64515603+AngieHuang5@users.noreply.github.com>
2021-12-19 00:36:50 -0800

    Update dbConn.php

--- dbConn.php
+++ dbConn.php
@@ -1,12 +1,21 @@
 <?php
+/*()
 $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "csp_can";
+*/
+/*
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "b1f0f895c5e0b5";
+$password = "67175e91";
+$dbname = "heroku_00fb331cfea2239";
+*/
 
-//echo "Hello!<br/>"
-
-//echo $servername . "<br/>"
+$servername = "us-cdbr-east-05.cleardb.net";
+$username = "b4dd2ee702817e";
+$password = "4c7b4665";
+$dbname = "heroku_9885c31f8807907";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
